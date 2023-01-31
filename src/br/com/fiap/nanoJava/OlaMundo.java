aea995916e62615449a0961fd63735feb838b89b
Giselle Hettwer Dias Dalazen <giselledias.joinville@gmail.com>
2023-01-31 16:01:06 -0300

    Aprendendo Variaveis

--- src/br/com/fiap/nanoJava/OlaMundo.java
+++ src/br/com/fiap/nanoJava/OlaMundo.java
@@ -2,4 +2,8 @@ package br.com.fiap.nanoJava;
 
 public class OlaMundo {
 
+	public static void main(String[] args) {
+		
+		System.out.println("FIAP - Olá mundo");
+	}
 }
---
+++ src/br/com/fiap/nanoJava/TiposVariaveis.java
@@ -0,0 +1,16 @@
+package br.com.fiap.nanoJava;
+
+public class TiposVariaveis {
+	public static void main(String[]args) {
+		int idade = 40;
+		double preco = 10.0;
+		char genero = 'F';
+		boolean maiorIdade = false;
+	
+		System.out.println(idade);
+		System.out.println(preco);
+		System.out.println(genero);
+		System.out.println(maiorIdade);
+	}
+	
+}
